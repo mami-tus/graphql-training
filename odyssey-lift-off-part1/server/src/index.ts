d3950c6a1d03ff99a6495442e8decb18b2fbd72c
mami.teshima <85916526+mami-tus@users.noreply.github.com>
2024-08-15 09:22:11 +0900

    feat: ✨ Lift-off I: Basics Apollo Server
    Backend first steps

--- odyssey-lift-off-part1/server/src/index.ts
+++ odyssey-lift-off-part1/server/src/index.ts
@@ -1,1 +1,17 @@
-//TODO
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './schema';
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs });
+  const { url } = await startStandaloneServer(server, {
+    // 4000 ポートすでに使われているので 4001 に変更
+    listen: { port: 4001 },
+  });
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+    `);
+}
+
+startApolloServer();

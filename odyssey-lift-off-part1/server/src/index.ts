9b166b491898bc4be47d95d77cc0a1a9d53d53c6
mami.teshima <85916526+mami-tus@users.noreply.github.com>
2024-08-15 09:28:15 +0900

    wip: mocking data

--- odyssey-lift-off-part1/server/src/index.ts
+++ odyssey-lift-off-part1/server/src/index.ts
@@ -1,9 +1,15 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './schema';
+import { addMocksToSchema } from '@graphql-tools/mock';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs });
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs }),
+    }),
+  });
   const { url } = await startStandaloneServer(server, {
     // 4000 ポートすでに使われているので 4001 に変更
     listen: { port: 4001 },

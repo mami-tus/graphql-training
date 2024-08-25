d6fc9a4dd185e1c4182b0c44905967e3b76e5589
mamitus <85916526+mami-tus@users.noreply.github.com>
2024-08-25 22:54:20 +0900

    8. Apollo Client setupまで

--- odyssey-lift-off-part1/client/src/index.tsx
+++ odyssey-lift-off-part1/client/src/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  // // 4000 ポートすでに使われているので 4001 にしている
+  uri: 'http://localhost:4001',
+  cache: new InMemoryCache(),
+});
 
 const root = createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
   </React.StrictMode>
 );

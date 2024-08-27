e5580ed3782c20fec13b43263bb02c6be178f9fa
mami.teshima <85916526+mami-tus@users.noreply.github.com>
2024-08-27 09:24:35 +0900

    Lift-off I: Basics > 9. Codegen

--- odyssey-lift-off-part1/client/codegen.ts
+++ odyssey-lift-off-part1/client/codegen.ts
@@ -1,18 +1,23 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  // // 4000 ポートすでに使われているので 4001 にしている
+  // GraphQLスキーマの場所 4000 ポートすでに使われているので 4001 にしている
   schema: 'http://localhost:4001',
+  // GraphQLのクエリやミューテーションが含まれているファイル
   documents: ['src/**/*.tsx'],
   generates: {
-    './src/generated/graphql.ts': {
-      plugins: [
-        'typescript',
-        'typescript-operations',
-        'typescript-react-apollo',
-      ],
+    // 生成されるファイルの出力先
+    './src/__generated__/': {
+      // クライアントサイドで利用するためのGraphQL型定義とクエリ実行関数を生成することを指定
+      preset: 'client',
+      // 生成されたコードでGraphQLクエリをgqlタグを使用して定義
+      presetConfig: {
+        gqlTagName: 'gql',
+      },
     },
   },
+  // クエリやミューテーションが見つからない場合でもエラーを発生させない
+  // ignoreNoDocuments: true,
 };
 
 export default config;

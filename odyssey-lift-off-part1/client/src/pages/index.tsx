4684427607be51db545fcfa66e893a480dea997a
mami.teshima <85916526+mami-tus@users.noreply.github.com>
2024-09-06 02:03:03 +0900

    11. The useQuery hook

--- odyssey-lift-off-part1/client/src/pages/index.tsx
+++ odyssey-lift-off-part1/client/src/pages/index.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 /** importing our pages */
-import Tracks from "./tracks";
+import Tracks from './tracks';
 
 export default function Pages() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Tracks />} path="/" />
+        <Route element={<Tracks />} path='/' />
       </Routes>
     </BrowserRouter>
   );
--- odyssey-lift-off-part1/client/src/pages/tracks.tsx
+++ odyssey-lift-off-part1/client/src/pages/tracks.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Layout } from '../components';
 import { gql } from '../__generated__';
 import { useQuery } from '@apollo/client';
+import TrackCard from '../containers/track-card';
+import QueryResult from '../components/query-result';
 
 /** TRACKS query to retrieve all tracks */
 const TRACKS = gql(`
@@ -28,11 +30,15 @@ const TRACKS = gql(`
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
 
-  if (loading) return 'Loading...';
-
-  if (error) return `Error! ${error.message}`;
-
-  return <Layout grid>{JSON.stringify(data)}</Layout>;
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
